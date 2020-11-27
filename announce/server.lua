bce63a47367d5ac49407536b84121af66123d4c6
CarterParker <54417014+CarterParker@users.noreply.github.com>
2020-11-27 14:34:52 -0500

    Update server.lua

--- announce/server.lua
+++ announce/server.lua
@@ -1,6 +1,6 @@
 -- script created by C. Parker, Join our discord for support! https://discord.gg/4A6Qdp9
-RegisterServerEvent('announce')
-addEventHandler('announce', function(param)
-    print('^7[^1Announcement^7]^5:'.. param)
-    TriggerClientEvent('chatMessage', -1, '^7[^1Announcement^7]^2', {0,0,0} --[[ this table is just rgb]], param)
+RegisterServerEvent("announce")
+AddEventHandler("announce", function(param)
+    print("^7[^1Announcement^7]^5:" .. param)
+    TriggerClientEvent("chatMessage", -1, "^7[^1Announcement^7]", {0,0,0}, param)
 end)
