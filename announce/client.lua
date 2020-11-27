bbe788a7d9ad6825924222d13cdaf3b04eded50f
CarterParker <54417014+CarterParker@users.noreply.github.com>
2020-11-27 14:34:33 -0500

    Update client.lua

--- announce/client.lua
+++ announce/client.lua
@@ -1,6 +1,5 @@
 -- script created by C. Parker, Join our discord for support! https://discord.gg/4A6Qdp9
+  
 RegisterCommand("announce", function(source, args)
-    TriggerServerEvent('announce', table.concat(args, " "))
-    -- we have to concatenate the table because the 'args' cb return a table (array)
-    -- the 2nd parameter in 'table.concat' is just spacing each args out
+    TriggerServerEvent("announce", table.concat(args, " "))
 end)
